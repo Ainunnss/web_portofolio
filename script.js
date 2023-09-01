a5c7d002a01fed5ce31b6f0d662c65004d64d85f
Ainunnss <142558358+Ainunnss@users.noreply.github.com>
2023-09-01 11:11:47 +0700

    latihan-web2

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('tes sudah terhubung');
+
+const navToggle = document.querySelector('.nav-toggle')
+const navMenu = document.querySelector('.nav-menu')
+
+navToggle.addEventListener('click', ()=> {
+    navMenu.classList.toggle('active');
+});
